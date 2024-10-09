63e10e03f8da68b09d1e771eaca61cadecb6bd79
Artem Sonin <artemijsonin471@gmail.com>
2024-10-09 22:33:05 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import CheckmarkBlock from './components/elememts/CheckmarkBlock/CheckmarkBlock';
 
 function App() {
+  const blockStyle = {
+    padding: '50px',
+    height: '100vh',
+    backgroundColor: 'black'
+  };  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={blockStyle}>
+      <CheckmarkBlock>Неограниченное колличество учеников</CheckmarkBlock>
+      <CheckmarkBlock>Трансляции</CheckmarkBlock>
+      <CheckmarkBlock>Точная статистика</CheckmarkBlock>
+      <CheckmarkBlock>Производительную видеосвязь с учениками </CheckmarkBlock>
+      <CheckmarkBlock>Продукт с ежемесячной подпиской</CheckmarkBlock>
+      <CheckmarkBlock>Лучшая конфиденциальность и защита</CheckmarkBlock>
+      <CheckmarkBlock>Готовый шаблон для организации обучения</CheckmarkBlock>
+      <CheckmarkBlock>Сообщества</CheckmarkBlock>
+      <CheckmarkBlock>Любое количество уроков и учебных программ</CheckmarkBlock>
     </div>
   );
 }
---
+++ src/components/elememts/CheckmarkBlock/CheckmarkBlock.js
@@ -0,0 +1,34 @@
+import checkmarkImage from '../../../Checkmark.png';
+
+function CheckmarkBlock({children}) {
+    const blockStyle = {
+        display: 'inline-flex',
+        gap: '30px',
+        alignItems: 'center',
+        padding: '20px 30px',
+        borderRadius: '84px',
+        margin: '0 30px 30px 0',
+        background: 'linear-gradient(rgba(243, 246, 239, .2), rgba(255, 255, 255, 1))',
+        width: 'fit-content'
+    };
+
+    const imageStyle = {
+        width: '40px',
+        height: '40px'
+    };
+
+    const textStyle = {
+        fontSize: '24px'
+    };
+
+    return (
+        <div style={blockStyle}>
+            <div style={textStyle}>
+                {children}
+            </div>
+            <img src={checkmarkImage} alt="Checkmark" style={imageStyle} />
+        </div>
+    );
+};
+
+export default CheckmarkBlock;
